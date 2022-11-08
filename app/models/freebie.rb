4145d6d7ecd97ee9b1c3ab275f05e70689e1597f
Colleen Cabedo <cmcabedo@gmail.com>
2022-11-07 20:47:19 -0500

    commit tree 6ca09c21cdaf0e0e61653749e0057eaab0fbca93

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 class Freebie < ActiveRecord::Base
+    belongs_to :dev
+    belongs_to :company
+
+    def print_details
+        binding.pry
+        # {insert dev's name} owns a {insert freebie's item_name} from {insert company's name}
+    end
 
 end
